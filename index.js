77e1b2aba2ed807d8efe92d28acdbadd5a2041d0
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-11-16 16:56:47 +1030

    adjust with class structures on all shapes

--- index.js
+++ index.js
@@ -53,9 +53,8 @@ inquirer
         },
     ])
     .then((answer) => {
-        shapes(answer);
-        console.log(shapes);
-        const shapeCode = shapes(answer);
+
+        
         const svg = `<svg version="1.1"
         width="300" height="200"
         xmlns="http://www.w3.org/2000/svg">
