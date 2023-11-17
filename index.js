a0d494a64500ae286bef07636dc964438d0a56f4
Create-88 <141834742+Create-88@users.noreply.github.com>
2023-11-18 09:40:31 +1030

    refactor code

--- index.js
+++ index.js
@@ -15,20 +15,16 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 //import classes of shapes
-const Shapes = require("./lib/shapes");
-const Triangle = require("./lib/triangle");
-const Square = require("./lib/square");
-const Circle = require("./lib/circle");
+const {Circle, Square, Triangle} = require("./lib/shapes");
+const Logo= require("./lib/logo");
+const { log } = require("console");
+
 
 //create array for shapeChoices
 const shapeChoices = ["circle", "square", "triangle"];
 
-// checkLength = () => {
-//     if (answer.characters.length > 3) {
-//         console.log("Logos only have up to 3 characters. Please try again.");
-//         return
-//     }
-// }
+
+
 
 inquirer
     .prompt([
@@ -36,16 +32,18 @@ inquirer
             type: "input",
             message: "What are the characters you would like on your logo? (maximum of 3 characters)",
             name: "characters",
+            validate:(characters)=> characters.length <=3 ||"Text cannot be more than 3"
         },
         {
             type: "input",
             message: "What colour would you like the text?",
             name: "textColour",
+           
         },
         {
             type: "list",
             message: "Which shape would you like your logo?",
-            name: "shape",
+            name: "shapeForm",
             choices: shapeChoices,
         },
         {
@@ -54,29 +52,39 @@ inquirer
             name: "shapeColour",
         },
     ])
-    .then((answer) => {
-        const shape = answer.shape;
-        console.log(answer.shape);
-        chooseFunction(shape);
-    //Stuck here with undefined shape
-        console.log(chooseFunction());
-        shape.setColor();
-        shape.render();
-        console.log(shape.render());
+    .then(({characters, textColour, shapeForm, shapeColour}) => {
+        let shape;
 
+        switch (shapeForm) {
+            case "circle":
+                shape= new Circle()
+                break;
+            case "square":
+                    shape= new Square()
+                    break;
+        
+            default:
+                shape= new Triangle()
+                break;
+        }
+     
+    
+        shape.setColor(shapeColour);
+        const logo= new Logo();
+        logo.setText(characters, textColour)
+        logo.setShape(shape)
+       
+       
+       fs.writeFileSync('./examples/logo.svg',logo.render())
 
-        // const svg = `<svg version="1.1"
-        // width="300" height="200"
-        // xmlns="http://www.w3.org/2000/svg">
-        // ${shapeCode}<text x="10" y="10" font-weight="bold" fill="${answer.textColour}">${answer.characters}</text>
-        // </svg>`
-        fs.writeFile('logo.svg', answer, (err) =>
-        err ? console.error(err)
-        : console.log('Generated logo.svg'));
-        console.log("Generated logo.svg");
+     
 
     })
+    .then(()=>{
+        console.log("Logo Generated");
+       })
     .catch((error) => {
+        console.log(error);
         if (error.isTtyError) {
             console.log("Prompt couldn't be rendered in current environment");
         } else {
@@ -84,16 +92,4 @@ inquirer
         }
     });
 
-function chooseFunction(shape) {
-    if(shape === "triangle") {
-        shape = new Triangle;
-        return shape;
-    } else if (shape === "square") {
-        shape = new Square;
-        return shape;
-    } else if (shape === "circle") {
-        shape = new Circle;
-        return shape;
-    }
-}
 
